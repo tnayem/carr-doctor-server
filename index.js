8211f53159cc410283f008c8a5e032a596467f6e
Md Tasdid Nayem <tasdidnayem@gmail.com>
2024-09-14 22:28:13 +0600

    implement get method for collect all services data

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(cors())
 app.use(express.json())
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ycg9h6w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,12 +25,19 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        const carServicesCollection = client.db('carDoctor').collection('services')
+        // Get Data from Database 
+        app.get('/services',async(req,res)=>{
+            // const data = req.body 
+            const result = await carServicesCollection.find().toArray()
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
