23b54450d96981b2b22054e3a4e076db1f794a5e
Md Tasdid Nayem <tasdidnayem@gmail.com>
2024-09-22 21:45:35 +0600

    add cors origin

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const port = process.env.PORT || 5000;
 const app = express()
 
 // Middleware 
-app.use(cors())
+app.use(cors({
+    origin:['http://localhost:5173'],
+    credentials:true
+}))
 app.use(express.json())
 
 
